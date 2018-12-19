e4d815d98c4d694592eeadbec116114315966114
yarkin <zql0114@gmail.com>
2018-12-19 15:23:21 +0800

    add micro block header

---
+++ src/struct/BlockData/BlockHeader/MicroBlockHeader.cpp
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2018 Zilliqa
+ * This source code is being disclosed to you solely for the purpose of your
+ * participation in testing Zilliqa. You may view, compile and run the code for
+ * that purpose and pursuant to the protocols and algorithms that are programmed
+ * into, and intended by, the code. You may not do anything else with the code
+ * without express permission from Zilliqa Research Pte. Ltd., including
+ * modifying or publishing the code (or any part of it), and developing or
+ * forming another public or private blockchain network. This source code is
+ * provided 'as is' and no warranties are given as to title or non-infringement,
+ * merchantability or fitness for purpose and, to the extent permitted by law,
+ * all liability for your use of the code is disclaimed. Some programs in this
+ * code are governed by the GNU General Public License v3.0 (available at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html) ('GPLv3'). The programs that
+ * are governed by GPLv3.0 are those programs that are located in the folders
+ * src/depends and tests/depends and which include a reference to GPLv3 in their
+ * program files.
+ */
+
+#include "MicroBlockHeader.h"
+#include "libMessage/Messenger.h"
+#include "libUtils/Logger.h"
+
+using namespace std;
+using namespace boost::multiprecision;
+
+MicroBlockHeader::MicroBlockHeader() { m_epochNum = (uint64_t)-1; }
+
+MicroBlockHeader::MicroBlockHeader(const vector<unsigned char>& src,
+                                   unsigned int offset) {
+  if (!Deserialize(src, offset)) {
+    LOG_GENERAL(WARNING, "We failed to init MicroBlockHeader.");
+  }
+}
+
+MicroBlockHeader::MicroBlockHeader(
+    uint8_t type, uint32_t version, uint32_t shardId, const uint256_t& gasLimit,
+    const uint256_t& gasUsed, const uint256_t& rewards,
+    const BlockHash& prevHash, const uint64_t& epochNum,
+    const uint256_t& timestamp, const MicroBlockHashSet& hashset,
+    uint32_t numTxs, const PubKey& minerPubKey, const uint64_t& dsBlockNum,
+    const CommitteeHash& committeeHash)
+    : BlockHeaderBase(committeeHash),
+      m_type(type),
+      m_version(version),
+      m_shardId(shardId),
+      m_gasLimit(gasLimit),
+      m_gasUsed(gasUsed),
+      m_rewards(rewards),
+      m_prevHash(prevHash),
+      m_epochNum(epochNum),
+      m_timestamp(timestamp),
+      m_hashset(hashset),
+      m_numTxs(numTxs),
+      m_minerPubKey(minerPubKey),
+      m_dsBlockNum(dsBlockNum) {}
+
+bool MicroBlockHeader::Serialize(vector<unsigned char>& dst,
+                                 unsigned int offset) const {
+  if (!Messenger::SetMicroBlockHeader(dst, offset, *this)) {
+    LOG_GENERAL(WARNING, "Messenger::SetMicroBlockHeader failed.");
+    return false;
+  }
+
+  return true;
+}
+
+bool MicroBlockHeader::Deserialize(const vector<unsigned char>& src,
+                                   unsigned int offset) {
+  if (!Messenger::GetMicroBlockHeader(src, offset, *this)) {
+    LOG_GENERAL(WARNING, "Messenger::GetMicroBlockHeader failed.");
+    return false;
+  }
+
+  return true;
+}
+
+const uint8_t& MicroBlockHeader::GetType() const { return m_type; }
+
+const uint32_t& MicroBlockHeader::GetVersion() const { return m_version; }
+
+const uint32_t& MicroBlockHeader::GetShardId() const { return m_shardId; }
+
+const uint256_t& MicroBlockHeader::GetGasLimit() const { return m_gasLimit; }
+
+const uint256_t& MicroBlockHeader::GetGasUsed() const { return m_gasUsed; }
+
+const uint256_t& MicroBlockHeader::GetRewards() const { return m_rewards; }
+
+const BlockHash& MicroBlockHeader::GetPrevHash() const { return m_prevHash; }
+
+const uint64_t& MicroBlockHeader::GetEpochNum() const { return m_epochNum; }
+
+const uint256_t& MicroBlockHeader::GetTimestamp() const { return m_timestamp; }
+
+const uint32_t& MicroBlockHeader::GetNumTxs() const { return m_numTxs; }
+
+const PubKey& MicroBlockHeader::GetMinerPubKey() const { return m_minerPubKey; }
+
+const uint64_t& MicroBlockHeader::GetDSBlockNum() const { return m_dsBlockNum; }
+
+const TxnHash& MicroBlockHeader::GetTxRootHash() const {
+  return m_hashset.m_txRootHash;
+}
+
+const StateHash& MicroBlockHeader::GetStateDeltaHash() const {
+  return m_hashset.m_stateDeltaHash;
+}
+
+const TxnHash& MicroBlockHeader::GetTranReceiptHash() const {
+  return m_hashset.m_tranReceiptHash;
+}
+
+const MicroBlockHashSet& MicroBlockHeader::GetHashes() const {
+  return m_hashset;
+}
+
+bool MicroBlockHeader::operator==(const MicroBlockHeader& header) const {
+  return std::tie(m_type, m_version, m_shardId, m_gasLimit, m_gasUsed,
+                  m_rewards, m_prevHash, m_epochNum, m_timestamp, m_hashset,
+                  m_numTxs, m_minerPubKey, m_dsBlockNum) ==
+         std::tie(header.m_type, header.m_version, header.m_shardId,
+                  header.m_gasLimit, header.m_gasUsed, header.m_rewards,
+                  header.m_prevHash, header.m_epochNum, header.m_timestamp,
+                  header.m_hashset, header.m_numTxs, header.m_minerPubKey,
+                  header.m_dsBlockNum);
+}
+
+bool MicroBlockHeader::operator<(const MicroBlockHeader& header) const {
+  return std::tie(header.m_type, header.m_version, header.m_shardId,
+                  header.m_gasLimit, header.m_gasUsed, header.m_rewards,
+                  header.m_prevHash, header.m_epochNum, header.m_timestamp,
+                  header.m_hashset, header.m_numTxs, header.m_minerPubKey,
+                  header.m_dsBlockNum) >
+         std::tie(m_type, m_version, m_shardId, m_gasLimit, m_gasUsed,
+                  m_rewards, m_prevHash, m_epochNum, m_timestamp, m_hashset,
+                  m_numTxs, m_minerPubKey, m_dsBlockNum);
+}
+
+bool MicroBlockHeader::operator>(const MicroBlockHeader& header) const {
+  return header < *this;
+}
---
+++ src/struct/BlockData/BlockHeader/MicroBlockHeader.h
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2018 Zilliqa
+ * This source code is being disclosed to you solely for the purpose of your
+ * participation in testing Zilliqa. You may view, compile and run the code for
+ * that purpose and pursuant to the protocols and algorithms that are programmed
+ * into, and intended by, the code. You may not do anything else with the code
+ * without express permission from Zilliqa Research Pte. Ltd., including
+ * modifying or publishing the code (or any part of it), and developing or
+ * forming another public or private blockchain network. This source code is
+ * provided 'as is' and no warranties are given as to title or non-infringement,
+ * merchantability or fitness for purpose and, to the extent permitted by law,
+ * all liability for your use of the code is disclaimed. Some programs in this
+ * code are governed by the GNU General Public License v3.0 (available at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html) ('GPLv3'). The programs that
+ * are governed by GPLv3.0 are those programs that are located in the folders
+ * src/depends and tests/depends and which include a reference to GPLv3 in their
+ * program files.
+ */
+
+#ifndef __MICROBLOCKHEADER_H__
+#define __MICROBLOCKHEADER_H__
+
+#include <array>
+#include <boost/multiprecision/cpp_int.hpp>
+
+#include "BlockHashSet.h"
+#include "BlockHeaderBase.h"
+#include "common/Constants.h"
+#include "common/Serializable.h"
+#include "libCrypto/Schnorr.h"
+
+/// Stores information on the header part of the microblock.
+class MicroBlockHeader : public BlockHeaderBase {
+  uint8_t m_type;  // 0: microblock proposed by a committee, 1: final tx block
+  uint32_t m_version;
+  uint32_t m_shardId;
+  boost::multiprecision::uint256_t m_gasLimit;
+  boost::multiprecision::uint256_t m_gasUsed;
+  boost::multiprecision::uint256_t m_rewards;
+  BlockHash m_prevHash;  // Hash of the previous block
+  uint64_t m_epochNum;   // Epoch Num
+  boost::multiprecision::uint256_t m_timestamp;
+  // TxnHash m_txRootHash; // Tx merkle tree root hash
+  // StateHash m_stateDeltaHash; // State Delta merkle tree root hash
+  MicroBlockHashSet m_hashset;
+  uint32_t m_numTxs;     // Total number of txs included in the block
+  PubKey m_minerPubKey;  // Leader of the committee who proposed this block
+  uint64_t
+      m_dsBlockNum;  // DS Block index at the time this Tx Block was proposed
+
+ public:
+  /// Default constructor.
+  MicroBlockHeader();
+
+  /// Constructor for loading existing microblock header from a byte stream.
+  MicroBlockHeader(const std::vector<unsigned char>& src, unsigned int offset);
+
+  /// Constructor with predefined member values.
+  MicroBlockHeader(const uint8_t type, const uint32_t version,
+                   const uint32_t shardId,
+                   const boost::multiprecision::uint256_t& gasLimit,
+                   const boost::multiprecision::uint256_t& gasUsed,
+                   const boost::multiprecision::uint256_t& rewards,
+                   const BlockHash& prevHash, const uint64_t& epochNum,
+                   const boost::multiprecision::uint256_t& timestamp,
+                   const MicroBlockHashSet& hashset, const uint32_t numTxs,
+                   const PubKey& minerPubKey, const uint64_t& dsBlockNum,
+                   const CommitteeHash& committeeHash);
+
+  /// Implements the Serialize function inherited from Serializable.
+  bool Serialize(std::vector<unsigned char>& dst, unsigned int offset) const;
+
+  /// Implements the Deserialize function inherited from Serializable.
+  bool Deserialize(const std::vector<unsigned char>& src, unsigned int offset);
+
+  // [TODO] These methods are all supposed to be moved into BlockHeaderBase, so
+  // no need to add Doxygen tags for now
+  const uint8_t& GetType() const;
+  const uint32_t& GetVersion() const;
+  const uint32_t& GetShardId() const;
+  const boost::multiprecision::uint256_t& GetGasLimit() const;
+  const boost::multiprecision::uint256_t& GetGasUsed() const;
+  const boost::multiprecision::uint256_t& GetRewards() const;
+  const BlockHash& GetPrevHash() const;
+  const uint64_t& GetEpochNum() const;
+  const boost::multiprecision::uint256_t& GetTimestamp() const;
+  const uint32_t& GetNumTxs() const;
+  const PubKey& GetMinerPubKey() const;
+  const uint64_t& GetDSBlockNum() const;
+  const TxnHash& GetTxRootHash() const;
+  const StateHash& GetStateDeltaHash() const;
+  const TxnHash& GetTranReceiptHash() const;
+  const MicroBlockHashSet& GetHashes() const;
+
+  // Operators
+  bool operator==(const MicroBlockHeader& header) const;
+  bool operator<(const MicroBlockHeader& header) const;
+  bool operator>(const MicroBlockHeader& header) const;
+};
+
+#endif  // __MICROBLOCKHEADER_H__
